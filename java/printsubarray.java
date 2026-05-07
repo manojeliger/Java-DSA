8abe9d366f6074bf7b81b73db964894d4c8096a8
Manoj Eliger <eligermanoj@gmail.com>
2026-05-07 15:18:59 +0530

    Added subarray printing program

---
+++ java/printsubarray.java
@@ -0,0 +1,23 @@
+public class printsubarray {
+   public static void subarray(int arr[]){
+
+    int n=arr.length-1;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n ; j++){
+            for(int k=i ; k<=j;k++){
+                System.out.print(arr[k]+" ");
+            } 
+            System.out.println(" ");
+        }
+    }
+   } 
+
+
+public void main(String args[]){
+
+    int arr[]={2,3,4,5,6};
+
+    subarray(arr);
+}
+}
+
